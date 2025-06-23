87d6f900c0d819ee9bad5237c3b78d9ac428023d
priyanshu singh <ishusingh4006@gmail.com>
2025-06-23 15:17:35 +0530

    Update the Home page adding two sub section , using the reusable componets,use the type-animation react library to show our text through live typing to inahance the user experience

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { FaArrowRight } from "react-icons/fa";
 import { HighlightText } from '../components/core/Home Page/HighlightText';
 import { CTAbutton } from '../components/core/Home Page/CTAbutton';
+import Banner from "../assets/Images/banner.mp4";
+import { CodeBlocks } from '../components/core/Home Page/CodeBlocks';
 export const Home = () => {
   return (
     <div>
@@ -28,18 +30,86 @@ export const Home = () => {
               <CTAbutton active={true} linkto={"/signup"} >Learn More</CTAbutton>
               <CTAbutton active={false} linkto={"login"}>Book a Demo</CTAbutton>
             </div>
-            <div className='shadow-blue-200 '>
+            <div className='mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200'>
                 <video
                  muted
                  autoPlay
-                 loop>
-                    
+                 loop
+                 className='shadow-[20px_20px_rgba(255,255,255)]'
+                 >
+                    <source src={Banner} type='video/mp4' />
                 </video>
+            </div>
+            {/* code section 1 */}
+            <div>
+              <CodeBlocks
+                position={"lg:flex-row"}
+                heading={
+                  <div className='text-4xl font-semibold'>
+                    Unlock Your <HighlightText text={"coding performance"}/> with our online courses.
+                  </div>
+                }
+                subHeading={
+                  "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+                }
+                ctabtn1={
+                  {
+                    btnText:"Try it Yourself" ,
+                    active:true,
+                    linkto:"/signup"
+                  }
+                }
+                ctabtn2={
+                  {
+                    btnText:"Learn More",
+                    active:false,
+                    linkto:"/login"
+                  }
+                }
+                codeblock={
+                  `<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`
+                }
+                codeColor={"text-yellow-25"}
+
+              />
+            </div>
+            {/* code section 2 */}
+            <div>
+              <CodeBlocks
+                position={"lg:flex-row-reverse"}
+                heading={
+                  <div className='text-4xl font-semibold'>
+                    Start  <HighlightText text={"coding in seconds"}/>
+                  </div>
+                }
+                subHeading={
+                  "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+                }
+                ctabtn1={
+                  {
+                    btnText:"Continue Lesson" ,
+                    active:true,
+                    linkto:"/signup"
+                  }
+                }
+                ctabtn2={
+                  {
+                    btnText:"Learn More",
+                    active:false,
+                    linkto:"/login"
+                  }
+                }
+                codeblock={
+                  `import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`
+                }
+                codeColor={"text-white"}
+
+              />
             </div>
 
         </div>
         {/* section2 */}
-
+  
         {/* section3 */}
 
         {/* Footer */}
